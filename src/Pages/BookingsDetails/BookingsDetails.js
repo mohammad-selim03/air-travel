228a918babc78c1f48b5ea7a7ef21fad4f36da11
Md. Selim Hossain <mrrafi812345@gmail.com>
2023-11-08 22:27:50 +0600

    message for icons

--- src/Pages/BookingsDetails/BookingsDetails.js
+++ src/Pages/BookingsDetails/BookingsDetails.js
@@ -1,83 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { FaAvianex, FaCat, FaPray, FaToolbox, FaUserAlt, FaUserMd, FaUserTie, FaUsers, FaWineGlassAlt } from 'react-icons/fa';
 import { useLoaderData } from 'react-router-dom';
+import { SlCup, SlPeople } from "react-icons/sl";
+
 
 const BookingsDetails = () => {
     const booking = useLoaderData();
     const { name, price, image, FlyingTime } = booking;
 
-    const [cart, setCart] = useState(0);
+    const [counter, setCounter] = useState(1);
 
+    const increaseNumber = () => {
+        setCounter((val) => val + 1);
+    }
 
-    const [value, setValue] = useState < Number > (args.value);
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setValue(v => v <= 0 ? args.value : v - 1);
-        }, 1000);
-        return () => {
-            clearTimeout(timer);
-        };
-    }, [value]);
-
+    const decreaseNumber = () => {
+        setCounter((val) => val - 1);
+    }
 
 
     return (
         <div>
-            <div className="hero min-h-screen" style={{ backgroundImage: `url(${image})` }}>
-                <div className="hero-overlay bg-opacity-60"></div>
-                <div className="hero-content text-center text-neutral-content">
-                    <div className="max-w-md">
-                        return <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
-                            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                                <Countdown className="font-mono text-5xl" value={15} />
-                                days
-                            </div>
-                            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                                <Countdown className="font-mono text-5xl" value={10} />
-                                hours
-                            </div>
-                            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                                <Countdown className="font-mono text-5xl" value={24} />
-                                min
-                            </div>
-                            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                                <Countdown className="font-mono text-5xl" value={value} />
-                                sec
-                            </div>
-                        </div>
-                        {/* <div className="grid grid-flow-col gap-5 text-center auto-cols-max justify-center items-center">
-                            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                                <span className="countdown font-mono text-5xl">
-                                    <span style={{ "--value": 15 }}></span>
-                                </span>
-                                days
-                            </div>
-                            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                                <span className="countdown font-mono text-5xl">
-                                    <span style={{ "--value": 10 }}></span>
-                                </span>
-                                hours
-                            </div>
-                            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                                <span className="countdown font-mono text-5xl">
-                                    <span style={{ "--value": 24 }}></span>
-                                </span>
-                                min
-                            </div>
-                            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                                <span className="countdown font-mono text-5xl">
-                                    <span style={{ "--value": 38 }}></span>
-                                </span>
-                                sec
-                            </div>
-                        </div> */}
-                        <h1 className="mb-5 text-5xl font-bold">{name}</h1>
-                        <p className="mb-5">Enjoy the best experience with us. We care our customer comfort and provide the best services.</p>
-                        <h4 className="text-xl font-bold mb-3">Flying Time : {FlyingTime}</h4>
-                        <h3 className="text-xl font-bold mb-5 flex justify-center items-center">Number of Members : <span className='text-3xl mx-3'><button className='bg-red-700 p-2 w-9 rounded-2xl'>-</button><button className='mx-3 bg-blue-700 p-2 rounded-2xl'>+</button></span> </h3>
-                        <h3 className="text-2xl font-bold mb-5">Price : {price}</h3>
-
-                        <button className="btn bg-gradient-to-r from-cyan-500 to-blue-800 border-blue-700 rounded font-bold text-white mt-3 mb-3">proceed to Payment</button>
-                    </div>
+            <div className='flex mx-auto'>
+                <img className='h-96 w-full' src={image} alt="" />
+            </div>
+            <div className="grid grid-cols-2 mt-5 mb-5">
+                <div>
+                    <h2 className="text-2xl font-bold">Our Services</h2>
+                    <ol className='font-semibold'>
+                        <li className='flex items-center'><SlCup className='mx-3 text-lg text-blue-700 font-bold' /> 1 time snack and heavy food</li>
+                        <li className='flex items-center'><SlCup className='mx-3 text-lg text-blue-700 font-bold' /><FaWineGlassAlt/> Complimentory Drinks</li>
+                        <li className='flex items-center'><FaCat  className='mx-3 text-lg text-blue-800 font-bold' /> Pet Allowed</li>
+                        <li><FaUserAlt /> Bussiness and Economy Class</li>
+                        <li><FaPray/> Prayer place</li>
+                        <li>Free travel for bellow 2 years children</li>
+                        <li><FaUserTie /> Best Pilots and Gentle Cabin Crue</li>
+                        <li><FaUserMd /> Free Primary Treatment</li>
+                    </ol>
+                </div>
+                <div>
+                    <h1 className="mb-5 text-3xl font-bold">{name}</h1>
+                    <p className="mb-5 font-semibold">Enjoy the best experience with us. We care our customer comfort and provide the best services.</p>
+                    <h4 className="text-lg font-semibold "><FaAvianex/> Flying Time : {FlyingTime}</h4>
+                    <h3 className="text-lg font-semibold mb-2 "><FaUsers/> Number of Members : <span className='text-3xl mx-3'>
+                        <button onClick={decreaseNumber} className='bg-red-700  w-7 h-10 rounded-2xl'>-</button>
+                        <button className='ml-2'>{counter}</button>
+                        <button onClick={increaseNumber} className='mx-3 bg-blue-700 w-7 h-10 rounded-2xl'>+</button></span> </h3>
+                    <h3 className="text-xl font-semibold mb-5">Price : {price}</h3>
+                    <button className="btn bg-gradient-to-r from-cyan-500 to-blue-800 border-blue-700 rounded font-bold text-white mt-3 mb-3">proceed to Payment</button>
                 </div>
             </div>
         </div>
